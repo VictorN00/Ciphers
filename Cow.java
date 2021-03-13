acd70ee5ec6c97b9a9b3f818006e8bef012e174a
Victor Nguyen <victor.bt.nguyen@gmail.com>
2021-03-12 19:36:26 -0600

    Update Cow.java

--- Cow.java
+++ Cow.java
@@ -1,14 +1,10 @@
 
-// Authors: Victor Nguyen, Roger Ye
-
 public class Cow {
 	
-	// Written by Roger Ye
 	public static String encode(String raw) {
 		return "cow";
 	}
 	
-	// Written by Victor Nguyen
 	public static String decode(String encrypted) {
 		return "tree";
 	}
